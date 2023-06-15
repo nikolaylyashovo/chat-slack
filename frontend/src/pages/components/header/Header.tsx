3982bf5292f11d15b6e234777a423ad91adb67b8
ivanlasov <lyashov@nwtn-group.com>
2023-06-15 22:15:52 +0300

    Revert "Revert "add dir project""
    This reverts commit b7eeea934c82f1c85a52b65fa42a221d051ecef9.

--- frontend/src/pages/components/header/Header.tsx
+++
@@ -1,9 +0,0 @@
-import classes from './header.module.css';
-
-export function Header() {
-  return (
-    <div className={classes.header}>
-      <h3 className={classes.text}>My Chat</h3>
-    </div>
-  )
-}
